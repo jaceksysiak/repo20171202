c36ea54ef92665b4da8c28d74cb849a0a01e2090
Jacek Sysiak <jaceksysiak@wp.pl>
2017-12-13 19:21:12 +0100

    20171213-1921

--- src/main/java/com/caveofprogramming/controllers/PageController.java
+++ src/main/java/com/caveofprogramming/controllers/PageController.java
@@ -2,11 +2,13 @@ package com.caveofprogramming.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.caveofprogramming.model.StatusUpdate;
 
 
 @Controller
 public class PageController {
-	 
 	@RequestMapping("/")
 	String home() {
 		return "app.homepage";
@@ -18,35 +20,14 @@ public class PageController {
 	}
 	
 	@RequestMapping("/addstatus")
-	String addStatus() {
-		return "app.addStatus";
+	ModelAndView addStatus(ModelAndView modelAndView) {
+		
+		modelAndView.setViewName("app.addStatus");
+		
+		StatusUpdate statusUpdate = new StatusUpdate();
+		
+		modelAndView.getModel().put("statusUpdate", statusUpdate);
+		
+		return modelAndView;
 	}
-	 
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/main/webapp/WEB-INF/tiles/home.jsp
+++ src/main/webapp/WEB-INF/tiles/home.jsp
@@ -1,4 +1,4 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 
-    Hello World from tiles 20171211 1142 !!!
+    Hello World from tiles!
