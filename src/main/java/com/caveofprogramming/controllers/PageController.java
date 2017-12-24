1df43a4e545ac32d4d732bf15786d0a0aac7c15f
Jacek Sysiak <jaceksysiak@wp.pl>
2017-12-24 16:18:13 +0100

    20171224-1618

--- src/main/java/com/caveofprogramming/controllers/PageController.java
+++ src/main/java/com/caveofprogramming/controllers/PageController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.caveofprogramming.model.StatusUpdate;
 import com.caveofprogramming.service.StatusUpdateService;
@@ -28,6 +29,20 @@ public class PageController {
 		return "app.about";
 	}
 
+	@RequestMapping(value="/viewstatus", method=RequestMethod.GET)
+	ModelAndView viewStatus(ModelAndView modelAndView, @RequestParam(name="p", defaultValue="1") Integer pageNumber){
+		
+
+		System.out.println();
+		System.out.println("==============" +pageNumber+ "==============");
+		System.out.println();
+		
+		modelAndView.setViewName("app.viewStatus");
+		
+		return modelAndView;
+	}
+	
+	
 	@RequestMapping(value = "/addstatus", method = RequestMethod.GET)
 	ModelAndView addStatus(ModelAndView modelAndView, @ModelAttribute("statusUpdate") StatusUpdate statusUpdate) {
 
--- src/main/webapp/WEB-INF/tiles/viewstatus.jsp
+++ src/main/webapp/WEB-INF/tiles/viewstatus.jsp
@@ -1,4 +1,4 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 
-    Hello World from tiles 2017-12-24 14:32 !!!
+page number: ${param.p}
